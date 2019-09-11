a1fc7510fac8d2096c95c6d1184da294e2301c70
Tomas Jovaišas <tomasjov@gmail.com>
2019-09-11 17:04:51 +0300

    Init

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $users = [
+            [
+                'id'             => 1,
+                'name'           => 'Admin',
+                'email'          => 'admin@admin.com',
+                'password'       => '$2y$10$qyxYm.2dlaXROvs0OrGHseo4qbeissRMqNWdhlcr/vUqE62vN94Fi',
+                'remember_token' => null,
+                'created_at'     => '2019-09-10 14:00:26',
+                'updated_at'     => '2019-09-10 14:00:26',
+            ],
+        ];
+
+        User::insert($users);
+
+    }
+}
